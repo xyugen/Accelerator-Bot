1197504a79ee09ff2df724696eb6ad7014cfb746
xyugen <68522615+xyugen@users.noreply.github.com>
2021-10-11 19:12:10 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,112 @@
+import discord
+from discord import user
+from discord import message
+from discord import activity
+from discord.ext import commands
+from discord.ext.commands import bot, Bot
+import random
+from discord.flags import Intents
+
+TOKEN = 'ODk0ODc4MDE0NjMzMDQxOTYx.YVwaGA.zdrCF-fBLvY8ap1Nzw2IEAapegY'
+
+client = discord.Client(activity=discord.Game(name='al;help'))
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(description="Yugen's creation. Current version: 1.0.", command_prefix='al;',
+                   intents=intents, allowed_mentions=discord.AllowedMentions(everyone=True))
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+global username
+global user_id
+global member
+global userAvatar
+
+@bot.event
+async def on_member_join(member):
+    username = member.name.split('#')[0]
+    userAvatar = member.avatar_url
+    channel = bot.get_channel(893315195831713803)
+    embed = discord.Embed(
+        title="Welcome!", description=f"{member.mention} just joined the server. Let us welcome them, @everyone!")
+    embed.set_author(name=f'{username} had joined!', icon_url=userAvatar)
+    await channel.send(embed=embed)
+
+@bot.event
+async def on_member_remove(member):
+    username = member.name.split('#')[0]
+    userAvatar = member.avatar_url
+    channel = bot.get_channel(893315195831713803)
+    embed = discord.Embed(
+        title="Goodbye!", description=f"{member.mention} just left the server. Let us say our goodbyes, @everyone.")
+    embed.set_author(name=f'{username} had left!', icon_url=userAvatar)
+    await channel.send(embed=embed)
+
+async def del_msg(msg):
+    await msg.message.delete()
+
+@bot.command(pass_context=True, name='Add', help="Adds two numbers")
+async def add(ctx, left: int, right: int):
+    await ctx.send(left + right)
+
+
+@bot.command(pass_context=True, name='Subtract', help="Substracts two numbers")
+async def subt(ctx, left: int, right: int):
+    await ctx.send(left - right)
+
+
+@bot.command(pass_context=True, name='Multiply', help="Multiplies two numbers")
+async def mult(ctx, left: int, right: int):
+    await ctx.send(left * right)
+
+
+@bot.command(pass_context=True, name='Divide', help="Divides two numbers")
+async def div(ctx, left: int, right: int):
+    await ctx.send(left / right)
+
+
+@bot.command(pass_context=True, name='Choices', help="Choose between given choices.")
+async def choose(ctx, *choices: str):
+    await ctx.send(random.choice(choices))
+
+
+@bot.command(pass_context=True, name='Member joined info', help="Checks the members date and time of joining the server.")
+async def joined(ctx, member: discord.Member):
+    await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
+
+
+@bot.command(pass_context=True, name='Repeat', help="Repeats and deletes your message.")
+async def repeat(ctx, *, arg):
+    user_id = ctx.author.id
+    channel = bot.get_channel(ctx.channel.id)
+    await ctx.send(f'<@{user_id}>: {arg}')
+    await del_msg(ctx)
+
+
+@bot.command(pass_context=True, name='Announce', help="Send your message to announcement channel.")
+async def announce(ctx, title, *, arg):
+    username = ctx.author.name.split('#')[0]
+    user_id = ctx.author.id
+    member = ctx.message.author
+    userAvatar = member.avatar_url
+    channel = bot.get_channel(893003734924271616)
+    embed = discord.Embed(title=title, description=arg, color=0xFF5733)
+    embed.set_author(name=username, icon_url=userAvatar)
+    await channel.send(embed=embed)
+
+
+@bot.command(pass_context=True, name='Tell', help='I will retell what you told me.')
+async def tell(ctx, *, arg):
+    user_id = ctx.author.id
+    embed=discord.Embed(title=f"{arg}", color=0xFF5733)
+    await ctx.send(embed=embed)
+    await del_msg(ctx)
+
+bot.run(TOKEN)
