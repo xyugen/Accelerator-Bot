ccad67dba5aa2f80640c954f815f65f4569ff6a6
xyugen <68522615+xyugen@users.noreply.github.com>
2021-10-12 00:25:03 +0800

    Update others.py

--- cogs/others.py
+++ cogs/others.py
@@ -13,6 +13,28 @@ class Other(commands.Cog):
         embed = discord.Embed(
             title='Pong!', description=f'My ping is {round(self.bot.latency * 1000)}ms.', color=0x0363ff)
         await ctx.send(embed=embed)
+        
+    @commands.command()
+    async def userinfo(self, ctx, *, user: discord.Member = None): # b'\xfc'
+        if user is None:
+            user = ctx.author      
+        date_format = "%a, %d %b %Y %I:%M %p"
+        embed = discord.Embed(color=0xdfa3ff, description=user.mention)
+        embed.set_author(name=str(user), icon_url=user.avatar_url)
+        embed.set_thumbnail(url=user.avatar_url)
+        embed.add_field(name="Joined", value=user.joined_at.strftime(date_format))
+        members = sorted(ctx.guild.members, key=lambda m: m.joined_at)
+        embed.add_field(name="Join position", value=str(members.index(user)+1))
+        embed.add_field(name="Registered", value=user.created_at.strftime(date_format))
+        if isinstance(ctx.channel, discord.DMChannel):
+            return
+        if len(user.roles) > 1:
+            role_string = ' '.join([r.mention for r in user.roles][1:])
+            embed.add_field(name="Roles [{}]".format(len(user.roles)-1), value=role_string, inline=False)
+        perm_string = ', '.join([str(p[0]).replace("_", " ").title() for p in user.guild_permissions if p[1]])
+        embed.add_field(name="Guild permissions", value=perm_string, inline=False)
+        embed.set_footer(text='ID: ' + str(user.id))
+        return await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Other(bot))
