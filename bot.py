a8a6d14daa293e824318a09435e7c623e63b0654
xyugen <68522615+xyugen@users.noreply.github.com>
2021-10-25 06:03:39 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,19 +47,24 @@ async def on_member_remove(member):
     await channel.send(embed=embed)
     
 badwords = ['fuck', 'shit', 'bitch', 'puta', 'gago', 'deputa', 'tangina', 'ulol', 'ulul', 'tanga', 'inutil', 'gunggong', 'gunggung', 'hunghang', 'tang ina', 'putangina', 'pota', 'potangina', 'putang ina', 'kingina', 'king ina']
+link = ['https://', 'http://']
 
 @bot.event
 async def on_message(message):
   if message.author.bot:
         return
   else:
-   for i in badwords: # Go through the list of bad words;
-      if i in (message.content.lower()):
-         await message.delete()
-         await message.channel.send(f"{message.author.mention}, please avoid using profanity.\n**Message deleted:** ||{message.content}||")
-         bot.dispatch('profanity', message, i)
-         return # So that it doesn't try to delete the message again, which will cause an error.
-   await bot.process_commands(message)
+    for l in link:
+        if l not in (message.lower()):
+            for i in badwords:  # Go through the list of bad words;
+                if i in (message.content.lower()):
+                    await message.delete()
+                    await message.channel.send(f"{message.author.mention}, please avoid using profanity.\n**Message deleted:** ||{message.content}||")
+                    bot.dispatch('profanity', message, i)
+                    # So that it doesn't try to delete the message again, which will cause an error.
+                    return
+    else:
+     await bot.process_commands(message)
 
 @bot.event
 async def on_command_error(ctx, error): #sends an error message when the entered command is not found
